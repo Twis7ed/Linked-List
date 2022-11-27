d1dea7c49790e14d502df2743f91f52d6ed11e98
Twisted <7w1571x@gmail.com>
2022-11-27 16:09:27 -0600

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -15,16 +15,14 @@ namespace tds
         Node* head;
         
     public:
-        list()
+        list (void) // EX: tds::list<int, 1> foo;
         {
-            if (Elements > 0)
-            {
+            if (Elements > 0) {
                 head = new Node;
                 Node* temp{ head };
                 Node* link{ head };
                 
-                for (int i{}; i < Elements -1; i++)
-                {
+                for (int i{}; i < Elements -1; i++) {
                     temp = new Node;
                     link->tail = temp;
                     link = link->tail;
@@ -38,16 +36,14 @@ namespace tds
             return;
         }
         
-        list (const std::initializer_list<T>& init)
+        list (const std::initializer_list<T>& init) // EX: tds::list<int, 1> foo{ 1 };
         {
-            if (Elements > 0)
-            {
+            if (Elements > 0) {
                 head = new Node;
                 Node* temp{ head };
                 Node* link{ head };
                 
-                for (int i{}; i < Elements -1; i++)
-                {
+                for (int i{}; i < Elements -1; i++) {
                     temp = new Node;
                     link->tail = temp;
                     link = link->tail;
@@ -56,8 +52,7 @@ namespace tds
                 temp->tail = nullptr;
                 temp = head;
                 
-                for (auto data : init)
-                {
+                for (auto data : init) {
                     temp->data = data;
                     temp = temp->tail;
                 }
@@ -68,19 +63,17 @@ namespace tds
             return;
         }
         
-        list (const list& copy)
+        list (const list& copy) // EX: tds::list<int, 1> goo{ foo };
         {
             Node* temp{ copy.head };
-            if (temp != nullptr)
-            {
+            if (temp != nullptr) {
                 head = new Node;
                 head->data = copy.head->data;
                 
                 Node* point{ head };
                 Node* link{ head };
                 
-                while (temp->tail != nullptr)
-                {
+                while (temp->tail != nullptr) {
                     point = new Node;
                     temp = temp->tail;
                     point->data = temp->data;
@@ -96,15 +89,13 @@ namespace tds
             return;
         }
         
-        int size()
+        int size (void) // Returns size of list, EX: list.size() - has value of int
         {
             int elements{};
             Node* temp{ head };
             
-            if (temp != nullptr)
-            {
-                while (temp != nullptr)
-                {
+            if (temp != nullptr) {
+                while (temp != nullptr) {
                     temp = temp->tail;
                     elements++;
                 }
@@ -113,11 +104,10 @@ namespace tds
             return elements;
         }
         
-        T& operator[] (const int& node)
+        T& operator[] (const int& node) // Allows for use of grabbing and setting values using []
         {
             Node* temp{ head };
-            if (temp != nullptr)
-            {
+            if (temp != nullptr) {
                 for (int i{}; i < node; i++)
                     temp = temp->tail;
             }
@@ -125,10 +115,9 @@ namespace tds
             return temp->data;
         }
         
-        ~list()
+        ~list (void) // Deconstructs list when finished
         {
-            while (head != nullptr)
-            {
+            while (head != nullptr) {
                 Node* temp{ head };
                 head = head->tail;
                 delete temp;
@@ -140,11 +129,10 @@ namespace tds
     };
 }
 
-int main() // Quick Test
+int main (void) // Quick Test
 {
     tds::list<int, 3> fib{ 0, 1 };
-    for (int i{}; i < 10; i++)
-    {
+    for (int i{}; i < 10; i++) {
         fib[2] = fib[0] + fib[1];
         std::cout << fib[0];
         
